496c7cb5fbb68c7dcb065c694ad5c676b05ccdc4
Rizel Bobb-Semple <rizel@botany.io>
2021-07-01 23:14:53 -0400

    still trying to debug on staging

--- src/components/SpringBig/SpringBig.js
+++ src/components/SpringBig/SpringBig.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { Button } from "grommet";
-// import axios from "axios";
+import axios from "axios";
+// import superagent  from "superagent";
 // import fetch from "node-fetch"
-import { HOSTNAME } from "../environment";
-import Modal from "react-modal";
+// import { HOSTNAME } from "../environment";
+// import Modal from "react-modal";
 import "./SpringBig.css";
 import styled from "styled-components";
-Modal.setAppElement("#root");
+// Modal.setAppElement("#root");
 
 const StyledButton = styled(Button)`
   background: #033fb4;
@@ -33,35 +34,56 @@ export default function SpringBig({ link }) {
     const options ={
       method: 'POST', 
       // url: 'https://api.springbig.technology/staging/pos/v1/members',
-      body: data, 
+      data: data, 
       headers: {
         'AUTH-TOKEN': '034b9e2bc45fdb39e00f99ec65f313d3',
         'X-api-key': 'b0bSI5WEa84daARcbYEXFPX0vMgT13R3Y5gPqKN2',
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Methods': 'GET, POST'
+        // 'Access-Control-Allow-Origin': '*',
+        // 'Content-Type': 'application/json',
+        // 'Access-Control-Allow-Methods': 'GET, POST'
       }
 }
     try {
-      const resp = await fetch('https://api.springbig.technology/staging/pos/v1/members', options);
-      console.log(resp)
-      return resp
+
+      // superagent
+      // .post('https://api.springbig.technology/staging/pos/v1/members')
+      // .send(data) // sends a JSON post body
+      // .set('AUTH-TOKEN', '034b9e2bc45fdb39e00f99ec65f313d3')
+      // .set('X-api-key', 'b0bSI5WEa84daARcbYEXFPX0vMgT13R3Y5gPqKN2')
+      // .set('accept', 'json')
+      // .end(function (err, res) {
+      //   console.log(err)
+      // });
+      // const resp = await axios('https://api.springbig.technology/staging/pos/v1/members', options);
+      // console.log(resp)
+      console.log('helllooooo')
+      // return resp
     } catch(err) {
      return err
     }
+  // }
+
+  // }
+    // try {
+    //   const resp = await axios('https://api.springbig.technology/staging/pos/v1/members', options);
+    //   console.log(resp)
+    //   return resp
+    // } catch(err) {
+    //  return err
+    // }
   }
   
-  function toggleModal() {
-    setShow(!show);
-  }
-  const url = new URL(link);
-  if (url.hostname === HOSTNAME) {
-    return (
-      <a target="_parent" href={link} rel="noreferrer">
-        {link}
-      </a>
-    );
-  } else {
+  // function toggleModal() {
+  //   setShow(!show);
+  // }
+  // const url = new URL(link);
+  // if (url.hostname === HOSTNAME) {
+  //   return (
+  //     <a target="_parent" href={link} rel="noreferrer">
+  //       {link}
+  //     </a>
+  //   );
+  // } else {
     return (
       <>
         <StyledButton
@@ -70,21 +92,21 @@ export default function SpringBig({ link }) {
           label="Become a VIP"
           primary
         />
-        <Modal
+        {/* <Modal
           isOpen={show}
           onRequestClose={toggleModal}
           contentLabel="My dialog"
           className="secondmodal"
-        >
+        > */}
           {/* <StyledHeader2>hello</StyledHeader2> */}
-          <iframe
+          {/* <iframe
             className="iframe_cover"
             src={link}
             style={{ width: "80%", height: "200px" }}
             title="springbig"
-          />
-        </Modal>
+          /> */}
+        {/* </Modal> */}
       </>
     );
-  }
+  // }
 }
