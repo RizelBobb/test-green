87f68b9837e1a27d93c1348669def774c530b975
Rizel Bobb-Semple <rizel@botany.io>
2021-07-19 15:53:28 -0400

    new title

--- src/components/LandingBanner/MobileLandingBanner/MobileLandingBanner.js
+++ src/components/LandingBanner/MobileLandingBanner/MobileLandingBanner.js
@@ -14,7 +14,7 @@ function MobileLandingBanner() {
     <Container id="home">
       <ComingSoonDiv>
         <StyledHeader1>
-          DELIVERY SERVICES <br></br>COMING SOON!
+          DELIVERIES<br></br>SERVICES<br></br>ARE LIVE!
         </StyledHeader1>
         <StyledHeader2>
           <StyledCheckCircle size={16} />
